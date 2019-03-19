4ec01afe3eb975bc92a9836e9195ec09875c90c3
KirstenBulsink <48523068+KirstenBulsink@users.noreply.github.com>
2019-03-20 00:44:30 +0100

    Update

--- visualisation_worldmap.py
+++ visualisation_worldmap.py
@@ -1,23 +1,19 @@
-
 # !!! for this script, first run the load data and preprocessing cells of main.py
 # for this you should also have the preprocessing module in the same directory
 # result: you should have the merged dataframe as variable
 
 #%%import necessary packages
 import networkx as nx
+import matplotlib as mpl
+
 
 #%% Create binary graph
 
-
-# selection of dataframe with unique combinations of [source airport ID - destination airport ID] 
-selection = df_merged.drop_duplicates(subset=["source airport ID", "destination airport ID"])
-selection2 = selection.iloc[:,2:21] # ignore airline info
-# but now include something that corrects for A to B == B to A
-# should sum up the amount of duplicates...
-
-# leave for now...
-
 # simple try:
+
+# create empty graph structure
+G = nx.Graph()
+
 # unique source and destination airports
 airport_source_uniq = df_merged["source airport ID"].unique() #3321 unique source airports
 airport_dest_uniq = df_merged["destination airport ID"].unique() #3327 unique dest airports
@@ -25,27 +21,42 @@ airport_dest_uniq = df_merged["destination airport ID"].unique() #3327 unique de
 # define nodes (now: source airports)
 nodes = airport_source_uniq.tolist()
 
-# create empty graph structure
-G = nx.Graph()
-
 # add nodes to graph
 G.add_nodes_from(nodes)
+# (Use keywords to update specific node attributes for every node, like size and weight)
 
-# Use keywords to update specific node attributes for every node, like size and weight
+# selection of dataframe with unique combinations of [source airport ID - destination airport ID] 
+selection_uniq_routes = df_merged.drop_duplicates(subset=["source airport ID", "destination airport ID"])
 
-# add edges, for example:
-G.add_edge(3329,6341)
-G.add_edge(3329,5811)
-G.add_edge(3329,5811)
+# create lists of source en dest airport IDs
+list_sourceAP_ID = selection_uniq_routes["source airport ID"].tolist()
+list_destAP_ID = selection_uniq_routes["destination airport ID"].tolist()
 
-# later:
-#define edges as a list of tuples
-#G.add_edges_from(edges)
+# define edges as list of tuples from these lists
+edges = list(zip(list_sourceAP_ID, list_destAP_ID))
+
+# add edges to graph
+G.add_edges_from(edges)
 
 # visualize graph (takes a while)
 nx.draw(G,with_labels=True)
-# cannot see the edges yet... 
-# maybe create better visualisation with deepgraph?
+nx.draw(G,with_labels=False)
+# x.draw(G,pos=nx.spring_layout(G)) diferent lay-out
+# does not yet show edges of graph..
+
+# try to visualise edges individually
+edge_graph = nx.draw_networkx_edges(G, pos=nx.spring_layout(G))
+
+#%%
+# other try of defining edges
+# define edges as possible correspondence between source airport ID and destination ID
+for node in nodes:
+    edges = (airport_source_uniq, airport_dest_uniq) 
+
+# add edges    
+G.add_edges_from([(airport_source_uniq, airport_dest_uniq)])
+# this doesn't work. should have list of tuples as input
+
 
 #%% How to:
 # create list of unique combinations
@@ -54,18 +65,35 @@ nx.draw(G,with_labels=True)
 # output: huge ass big crazy graph 
 
 
+# selection of dataframe with unique combinations of [source airport ID - destination airport ID] 
+selection = df_merged.drop_duplicates(subset=["source airport ID", "destination airport ID"])
+selection2 = selection.iloc[:,2:21] # ignore airline info
+# but now include something that corrects for A to B == B to A
+# should sum up the amount of duplicates...
 
 
-#%% (create weighted)
+
+# change node size depending on degree node
+d = nx.degree(G)
+print(d)
+
+nx.draw(G, nodelist=d.keys(), node_size=[v * 100 for v in d.values()])
+plt.show()
+
+
+
+# visualise with matplotlib - does not work...
+import matplotlib.pyplot as plt
+#mpl.pyplot.show()
+plt.savefig("graph.png")
+plt.show()
+
+
+
+#%% create weighted graph
 
 # create list of unique combinations
 # every unique combination of [soure airport id - destination airport id]. But then correct for A to B == B to A
 
 # this is in matlab: [outputname, IA, IC] = unique(var)
 # probably .unique also holds something like this in python
-
-
-#%% Visualisation on a word map (use binary!)
-
-# take coordinates as location for the hubs of the binary graph
-
